b5b0e4154cd62d3be48c051b0f9e9de357f3615f
資訊部 林亞萱 <A1130405@jan-cheng.corp>
2024-11-07 15:14:47 +0800

    切換到main分支，重新上傳

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <h1></h1>
       </nav>
     </div>
   </header>
